51f6d3b8ce17f625dbe31ee5e3d18daba1caa188
mikko@sorl.net@18c32c52-eb3f-0410-93bd-f15eff1430f4 <mikko@sorl.net@18c32c52-eb3f-0410-93bd-f15eff1430f4>
2007-11-29 07:37:23 +0000

    ...

--- thumbnail/templatetags/thumbnail.py
+++ thumbnail/templatetags/thumbnail.py
@@ -20,16 +20,10 @@ def thumbnail_url(filename, arg=""):
 
 def get_thumbnail(filename, arg=""):
 
-    kwargs  = {\
-        'prefix': '',
-        'subdir': '_thumbs',
-        'quality': 85,
-    }
-
+    kwargs  = {'prefix': '', 'subdir': '_thumbs', 'quality': 85}
     for a in kwargs.keys():
         if hasattr(settings, 'THUMBNAIL_%s' % a.upper()):
             kwargs.update({a: getattr(settings, 'THUMBNAIL_%s' % a.upper())})
-    
     kwargs.update({'filename': filename, 'size': (80,80)})
      
     for m in METHOD_LIST:
