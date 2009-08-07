6dc7124e2815515cf28d9fd61c2c947e54fee5d4
smileychris@18c32c52-eb3f-0410-93bd-f15eff1430f4 <smileychris@18c32c52-eb3f-0410-93bd-f15eff1430f4>
2009-08-07 12:02:26 +0000

    Fixed issue 83 - setup.py not installing management commands

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from distutils.core import setup
  
 setup(
-    name = "sorl",
+    name = "sorl-thumbnail",
     version = "3.1",
     packages = [
         "sorl",
         "sorl.thumbnail",
         "sorl.thumbnail.templatetags",
         "sorl.thumbnail.tests",
+        "sorl.thumbnail.management",
+        "sorl.thumbnail.management.commands",
     ],
 )
