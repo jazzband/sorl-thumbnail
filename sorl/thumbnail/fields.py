74d9175c5a7ad63881432cdffccaff69ce95dabe
Chris Beaven <smileychris@gmail.com>
2009-09-15 22:09:57 +1200

    Tidy up the code style from merge.

--- sorl/thumbnail/fields.py
+++ sorl/thumbnail/fields.py
@@ -171,8 +171,11 @@ class BaseThumbnailField(ImageField):
             for extra, attrs in self.extra_thumbnails.items():
                 name = "%r of 'extra_thumbnails'"
                 _verify_thumbnail_attrs(attrs, name)
+
     def south_field_triple(self):
-        "Returns a suitable description of this field for South."
+        """
+        Return a suitable description of this field for South.
+        """
         # We'll just introspect ourselves, since we inherit.
         from south.modelsinspector import introspector
         field_class = "django.db.models.fields.files.ImageField"
