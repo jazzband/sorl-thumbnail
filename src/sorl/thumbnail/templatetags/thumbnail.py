4441d0db97dec53623c8e1fd6704dbdf965429d5
Mikko Hellsing <mikko@aino.se>
2010-10-26 04:42:41 +0200

    thumbs up

--- src/sorl/thumbnail/templatetags/thumbnail.py
+++ src/sorl/thumbnail/templatetags/thumbnail.py
@@ -1,11 +1,13 @@
 import re
 from django.template import Library, Node, TemplateSyntaxError
 from django.utils.encoding import smart_str
+from sorl.thumbnail.helpers import get_thumbnail
 from sorl.thumbnail.conf import settings
-from sorl.thumbnail.base import get_thumbnailfile
 
 
 register = Library()
+
+
 kw_pat = re.compile(r'^(?P<key>[\w]+)=(?P<value>.+)$')
 integer_pat = re.compile(r'^\d+$')
 bool_pat = re.compile(r'^(True|False)$')
@@ -21,14 +23,14 @@ class ThumbnailNode(Node):
             raise syntax_error()
         self.file_ = parser.compile_filter(bits[1])
         self.geometry = parser.compile_filter(bits[2])
-        self.kwargs = {}
+        self.options = {}
         for bit in bits[2:-3]:
             m = kw_pat.match(bit)
             if not m:
                 raise syntax_error()
             key = smart_str(m.group('key'))
             value = parser.compile_filter(m.group('value'))
-            self.kwargs[key] = value
+            self.options[key] = value
         self.as_var = bits[-1]
         self.nodelist = parser.parse(('endthumbnail',))
         parser.delete_first_token()
@@ -39,22 +41,16 @@ class ThumbnailNode(Node):
         except Exception:
             if settings.THUMBNAIL_DEBUG:
                 raise
+            context[self.as_var] = None # reset in case it was set in a loop
             return settings.THUMBNAIL_ERROR
 
     def _render(self, context):
         file_ = self.file_.resolve(context)
         geometry = self.geometry.resolve(context)
-        kwargs = {}
-        for key, value in self.kwargs.iteritems():
-            value = value.resolve(context)
-            # make basestrings with int contents be ints
-            if isinstance(value, basestring) and integer_pat.match(value):
-                value = int(value)
-            # make basestrings with bool contents be bools
-            elif isinstance(value, basestring) and bool_pat.match(value):
-                value = int(value)
-            kwargs[key] = value
-        thumbnail = get_thumbnailfile(file_, geometry, **kwargs)
+        options = {}
+        for key, value in self.options.iteritems():
+            options[key] = value.resolve(context)
+        thumbnail = get_thumbnail(file_, geometry, **options)
         context.push()
         context[self.as_var] = thumbnail
         output = self.nodelist.render(context)
