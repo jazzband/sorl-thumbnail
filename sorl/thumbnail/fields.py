94b641da925e5572af00e0ed202e53a2b6088638
Mikko Hellsing <mikko@aino.se>
2010-12-16 02:20:51 +0100

    all is all

--- sorl/thumbnail/fields.py
+++ sorl/thumbnail/fields.py
@@ -6,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from sorl.thumbnail import default
 
 
-__all__ = ('ImageField', 'ImageFormField')
+__all__ = ('ImageField', 'ImageFormField', 'ClearableImageFormField')
 
 
 class South(object):
