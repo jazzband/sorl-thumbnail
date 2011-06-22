ea385fd8718d017ddfedf0be5f2fe9c59e1556f2
Henrik Almer <henrik@agoodid.se>
2011-06-22 14:31:19 +0200

    Added a ``db_column`` property to the key and valye fields in the KVStore model
    This makes sorl-thumbnail support a wider variety of database systems, including MS SQL Server, by using a column name that does not conflict with a reserved keyword.

--- sorl/thumbnail/models.py
+++ sorl/thumbnail/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class KVStore(models.Model):
-    key = models.CharField(max_length=200, primary_key=True)
-    value = models.TextField()
+    key = models.CharField(max_length=200, primary_key=True, db_column='key_field')
+    value = models.TextField(db_column='value_field')
 
