c37de48d6fed8353c312787608407ab127de960f
Mikko Hellsing <mikko@aino.se>
2011-09-09 05:23:22 +0200

    tests work

---
+++ tests/settings.py
@@ -0,0 +1,29 @@
+from os.path import join as pjoin, abspath, dirname
+
+
+PROJ_ROOT = abspath(dirname(__file__))
+THUMBNAIL_PREFIX = 'test/cache/'
+THUMBNAIL_DEBUG = True
+THUMBNAIL_LOG_HANDLER = {
+    'class': 'sorl.thumbnail.log.ThumbnailLogHandler',
+    'level': 'ERROR',
+}
+ADMINS = (
+    ('Sorl', 'thumbnail@sorl.net'),
+)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'thumbnail_tests',
+    }
+}
+MEDIA_ROOT = pjoin(PROJ_ROOT, 'media')
+MEDIA_URL = '/media/'
+ROOT_URLCONF = 'tests.urls'
+INSTALLED_APPS = (
+    'thumbnail',
+    'thumbnail_tests',
+)
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.request",
+)
