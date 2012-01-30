3f65567487dbc1fd63d309358fa8f9360d02804e
outi <marcin.koziol@codepill.com>
2012-01-30 12:11:30 +0100

    using models.ImageField as base class for ImageField for easier validation

--- sorl/thumbnail/fields.py
+++ sorl/thumbnail/fields.py
@@ -9,7 +9,7 @@ from sorl.thumbnail import default
 __all__ = ('ImageField', 'ImageFormField')
 
 
-class ImageField(models.FileField):
+class ImageField(models.ImageField):
     def delete_file(self, instance, sender, **kwargs):
         """
         Adds deletion of thumbnails and key kalue store references to the
