6e92468f0c06d0486feac142d9396fdff89acf02
x0nix <jakub.dolejsek@s-cape.cz>
2012-06-12 18:11:05 +0300

    Catch KeyError as well

--- sorl/thumbnail/engines/pil_engine.py
+++ sorl/thumbnail/engines/pil_engine.py
@@ -27,7 +27,7 @@ class Engine(EngineBase):
     def _orientation(self, image):
         try:
             exif = image._getexif()
-        except (IndexError, AttributeError):
+        except (IndexError, AttributeError, KeyError):
             exif = None
         if exif:
             orientation = exif.get(0x0112)
