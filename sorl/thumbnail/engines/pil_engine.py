21e3222fd1bc3fcac9227e8ec563779dd5ac79d0
Philip Neustrom <philipn@gmail.com>
2013-06-18 17:28:28 -0700

    Fixes #112

--- sorl/thumbnail/engines/pil_engine.py
+++ sorl/thumbnail/engines/pil_engine.py
@@ -27,7 +27,7 @@ class Engine(EngineBase):
     def _orientation(self, image):
         try:
             exif = image._getexif()
-        except AttributeError:
+        except (AttributeError, IOError):
             exif = None
         if exif:
             orientation = exif.get(0x0112)
