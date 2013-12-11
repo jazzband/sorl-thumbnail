bc11a1b122d6f101e698654b4ed9c6ba655be235
Rolf Erik Lekang <me@rolflekang.com>
2013-12-11 21:47:15 +0100

    use strings instead of unicode literals in log

--- tests/thumbnail_tests/tests.py
+++ tests/thumbnail_tests/tests.py
@@ -53,7 +53,7 @@ class StorageTestCase(unittest.TestCase):
         slog.start_log()
         get_thumbnail(self.im, '50x50')
         log = slog.stop_log()
-        self.assertEqual(log, []) # now this should all be in cache
+        self.assertEqual(log, [])  # now this should all be in cache
 
     def test_c_safe_methods(self):
         slog.start_log()
