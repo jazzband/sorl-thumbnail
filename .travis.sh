b3e81a60b9cb872bf7f5d26f380adb668b15c208
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-01-10 02:05:37 -0400

    Use correct Pillow wheel for python3

---
+++ .travis.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+REPO_WHEEL=https://github.com/mariocesar/sorl-thumbnail/raw/wheels/wheel
+
+pip install Wheel
+
+if $python --version 2>&1 >/dev/null | grep -q 'Python 3';
+    pip install $REPO_WHEEL/Pillow-2.3.0-cp32-cp32mu-linux_x86_64.whl;
+else
+    pip install $REPO_WHEEL/Pillow-2.3.0-cp27-none-linux_x86_64.whl;
+fi
+
+pip install $PIP
+
+pip install django==$DJANGO_VERSION
+
+pip install coveralls
