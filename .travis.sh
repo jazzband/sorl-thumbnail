12698484bb81cae31daab1d804453d1d38edab12
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-01-10 02:07:47 -0400

    Fix bash syntax

--- .travis.sh
+++ .travis.sh
@@ -4,7 +4,7 @@ REPO_WHEEL=https://github.com/mariocesar/sorl-thumbnail/raw/wheels/wheel
 
 pip install Wheel
 
-if $python --version 2>&1 >/dev/null | grep -q 'Python 3';
+if $python --version 2>&1 >/dev/null | grep -q 'Python 3'; then
     pip install $REPO_WHEEL/Pillow-2.3.0-cp32-cp32mu-linux_x86_64.whl;
 else
     pip install $REPO_WHEEL/Pillow-2.3.0-cp27-none-linux_x86_64.whl;
