aad840bc01cf4396995b47b891c1a137a14fe16f
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-02-09 18:46:47 -0400

    Disable wheel package for Pillow to get Travis working

--- .travis.sh
+++ .travis.sh
@@ -4,11 +4,13 @@ REPO_WHEEL=http://github.com/mariocesar/sorl-thumbnail/raw/wheels/wheel
 
 pip install Wheel
 
-if python --version 2>&1 >/dev/null | grep -q 'Python 3'; then
-    pip install Pillow;
-else
-    pip install $REPO_WHEEL/Pillow-2.3.0-cp27-none-linux_x86_64.whl;
-fi
+#if python --version 2>&1 >/dev/null | grep -q 'Python 3'; then
+#    pip install Pillow;
+#else
+#    pip install $REPO_WHEEL/Pillow-2.3.0-cp27-none-linux_x86_64.whl;
+#fi
+
+pip install Pillow
 
 pip install $PIP
 
