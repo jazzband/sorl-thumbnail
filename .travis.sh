fb7ab61de1c15c793ce93271590dd1848ebc5c0b
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-02-13 08:47:29 -0400

    Make sure future upgrade of django will be used. Enable wheels again

--- .travis.sh
+++ .travis.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 
-REPO_WHEEL=http://github.com/mariocesar/sorl-thumbnail/raw/wheels/wheel
+REPO_WHEEL=https://raw.github.com/mariocesar/sorl-thumbnail/wheels/wheel
+
+pip install -U pip
 
 pip install Wheel
 
-#if python --version 2>&1 >/dev/null | grep -q 'Python 3'; then
-#    pip install Pillow;
-#else
-#    pip install $REPO_WHEEL/Pillow-2.3.0-cp27-none-linux_x86_64.whl;
-#fi
+if python --version 2>&1 >/dev/null | grep -q 'Python 3'; then
+    pip install Pillow;
+else
+    pip install $REPO_WHEEL/Pillow-2.3.0-cp27-none-linux_x86_64.whl;
+fi
 
 pip install Pillow
 
 pip install $PIP
 
-pip install django==$DJANGO_VERSION
+pip install Django$DJANGO_VERSION
 
 pip install coveralls
