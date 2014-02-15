a552f74b4626ac9aa189dccdd4141cea92d6ab41
Saul <s.shanabrook@gmail.com>
2014-02-15 08:37:34 -0400

    Added option to get redis KV store from redis url #149
        Used in Redis To Go in Heroku
        https://devcenter.heroku.com/articles/redistogo#using-from-python

--- sorl/thumbnail/kvstores/redis_kvstore.py
+++ sorl/thumbnail/kvstores/redis_kvstore.py
@@ -1,4 +1,4 @@
-from redis import Redis
+import redis
 from sorl.thumbnail.kvstores.base import KVStoreBase
 from sorl.thumbnail.conf import settings
 
@@ -6,13 +6,17 @@ from sorl.thumbnail.conf import settings
 class KVStore(KVStoreBase):
     def __init__(self, *args, **kwargs):
         super(KVStore, self).__init__(*args, **kwargs)
-        self.connection = Redis(
-            host=settings.THUMBNAIL_REDIS_HOST,
-            port=settings.THUMBNAIL_REDIS_PORT,
-            db=settings.THUMBNAIL_REDIS_DB,
-            password=settings.THUMBNAIL_REDIS_PASSWORD,
-            unix_socket_path=settings.THUMBNAIL_REDIS_UNIX_SOCKET_PATH,
-        )
+
+        if hasattr(settings, 'THUMBNAIL_REDIS_URL'):
+            self.connection = redis.from_url(settings.THUMBNAIL_REDIS_URL)
+        else:
+            self.connection = redis.Redis(
+                host=settings.THUMBNAIL_REDIS_HOST,
+                port=settings.THUMBNAIL_REDIS_PORT,
+                db=settings.THUMBNAIL_REDIS_DB,
+                password=settings.THUMBNAIL_REDIS_PASSWORD,
+                unix_socket_path=settings.THUMBNAIL_REDIS_UNIX_SOCKET_PATH,
+            )
 
     def _get_raw(self, key):
         return self.connection.get(key)
