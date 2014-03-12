2a4a5bceb352259f9bc68c69ab4a089917a85922
Mario César <mariocesar@creat1va.com>
2014-03-12 11:45:57 -0700

    Preparing new release [skip ci]

--- sorl/__init__.py
+++ sorl/__init__.py
@@ -5,7 +5,7 @@ import logging
 
 __author__ = "Mikko Hellsing"
 __license__ = "BSD"
-__version__ = '12.0'
+__version__ = '13.0'
 __maintainer__ = "Mario César Señoranis Ayala"
 __email__ = "mariocesar@creat1va.com"
 __status__ = "Production"
