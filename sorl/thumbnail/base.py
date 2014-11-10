df692491b0c6a1334168e9c6ed330bc1cc881a15
Guillermo Colmenero <guillermocolmenero@gmail.com>
2014-11-10 16:56:08 -0200

    Adding GIF support.

--- sorl/thumbnail/base.py
+++ sorl/thumbnail/base.py
@@ -15,6 +15,7 @@ logger = logging.getLogger(__name__)
 EXTENSIONS = {
     'JPEG': 'jpg',
     'PNG': 'png',
+    'GIF': 'gif',
 }
 
 
@@ -52,6 +53,8 @@ class ThumbnailBackend(object):
             return 'JPEG'
         elif file_extension == '.png':
             return 'PNG'
+        elif file_extension == '.gif':
+            return 'GIF'
         else:
             from django.conf import settings
 
