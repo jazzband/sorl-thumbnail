3541db7f4160a126eecd8441348390955e5b7779
Caio Ariede <caio.ariede@gmail.com>
2015-08-01 15:59:32 -0300

    Uses the NamedTemporaryFile class provided by Django

--- sorl/thumbnail/engines/convert_engine.py
+++ sorl/thumbnail/engines/convert_engine.py
@@ -2,9 +2,9 @@ from __future__ import unicode_literals, with_statement
 import re
 import os
 import subprocess
-from tempfile import NamedTemporaryFile
 
 from django.utils.encoding import smart_str
+from django.core.files.temp import NamedTemporaryFile
 
 from sorl.thumbnail.base import EXTENSIONS
 from sorl.thumbnail.compat import b
