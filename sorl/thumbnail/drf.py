48f3992aa4b6a7950dc490a00055418cfd1e71f6
Dulmandakh <dulmandakh@gmail.com>
2018-11-10 14:58:12 +0800

    ImageField for Django Rest Framework

---
+++ sorl/thumbnail/drf.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from sorl.thumbnail.default import backend
+
+THUMBNAIL_OPTIONS = set(backend.default_options) | set(k for k, v in backend.extra_options)
+
+class ImageField(serializers.ImageField):
+    def __init__(self, resize=None, *args, **kwargs):
+        self.resize = resize
+        if self.resize:
+            self.thumbnail_options = {
+                key: kwargs.pop(key)
+                for key in THUMBNAIL_OPTIONS if key in kwargs
+            }
+        super().__init__(*args, **kwargs)
+
+
+    def to_representation(self, value):
+        if self.resize and value:
+            return get_thumbnail(value, self.resize, **self.thumbnail_options).url
+        return super().to_representation(value)
