22ccd9781462a820f963f57018ad3dcef85053ed
Tim Graham <timograham@gmail.com>
2019-01-23 19:18:14 -0500

    Add testing for Django 2.2

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
         'Framework :: Django :: 1.11',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
     ],
     cmdclass={"test": TestCommand},
 )
