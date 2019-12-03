db43470c05a4a08abb5828c5725626fcd88d707f
Hugo Des <hugo.deslongchamps@gmail.com>
2019-12-03 11:10:39 +0100

    Add Django 3 support

--- sorl/thumbnail/models.py
+++ sorl/thumbnail/models.py
@@ -1,5 +1,5 @@
+from six import python_2_unicode_compatible
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
 
 from sorl.thumbnail.conf import settings
 
