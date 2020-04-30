3e02665645d7135c5efc3434ad5b04586220b570
Régis Behmo <regis@behmo.com>
2020-05-01 00:45:37 +0200

    Simpler settings management
    The previous settings structure was causing the loading of all django
    settings, including those that are deprecated. This was causing
    deprecation warnings for most users. These warnings could be disabled at
    the cost of hiding all django deprecation warnings, which was too bad.
    
    We took the opportunity to get rid of a now useless test utility:
    "override_custom_settings".
    
    Close #589.

--- tests/thumbnail_tests/utils.py
+++ tests/thumbnail_tests/utils.py
@@ -6,8 +6,6 @@ from contextlib import contextmanager
 from subprocess import check_output
 
 from PIL import Image, ImageDraw
-from django.test.signals import setting_changed
-from django.conf import UserSettingsHolder
 
 from sorl.thumbnail.conf import settings
 from sorl.thumbnail.helpers import get_module_class
@@ -46,41 +44,6 @@ def get_open_fds_count():
         [s for s in procs.decode('utf-8').split('\n') if s and s[0] == 'f' and s[1:].isdigit()]
     )
     return nprocs
-
-
-class override_custom_settings(object):
-    """
-    settings overrider context manager.
-    https://github.com/django/django/blob/1.6.2/django/test/utils.py#L209-L268
-    """
-
-    def __init__(self, settings_obj, **kwargs):
-        self.settings = settings_obj
-        self.options = kwargs
-
-    def __enter__(self):
-        self.enable()
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.disable()
-
-    def enable(self):
-        override = UserSettingsHolder(self.settings._wrapped)
-        for key, new_value in self.options.items():
-            setattr(override, key, new_value)
-        self.wrapped = self.settings._wrapped
-        self.settings._wrapped = override
-        for key, new_value in self.options.items():
-            setting_changed.send(sender=self.settings._wrapped.__class__,
-                                 setting=key, value=new_value, enter=True)
-
-    def disable(self):
-        self.settings._wrapped = self.wrapped
-        del self.wrapped
-        for key in self.options:
-            new_value = getattr(self.settings, key, None)
-            setting_changed.send(sender=self.settings._wrapped.__class__,
-                                 setting=key, value=new_value, enter=False)
 
 
 class FakeFile(object):
