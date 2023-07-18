35741d75e664099b2b35eb050e23cfcd157867ae
LarryMartell <larry@software-horizons.com>
2023-07-18 07:59:11 -0700

    fixed DeprecationWarning

--- sorl/__init__.py
+++ sorl/__init__.py
@@ -1,6 +1,7 @@
-from pkg_resources import get_distribution, DistributionNotFound
-
 try:
-    __version__ = get_distribution("sorl-thumbnail").version
-except DistributionNotFound:
-    pass
+    # Python 3.8+
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    import importlib_metadata
+
+__version__ = importlib_metadata.version("sorl-thumbnail")
