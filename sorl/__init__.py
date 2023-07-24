1ef258ee8bab40dda4f9c92141e65bff19f6e57d
LarryMartell <larry@software-horizons.com>
2023-07-24 11:20:59 -0700

    updates for tests

--- sorl/__init__.py
+++ sorl/__init__.py
@@ -1,7 +1,11 @@
-try:
-    # Python 3.8+
-    import importlib.metadata as importlib_metadata
-except ImportError:
-    import importlib_metadata
+import sys
 
-__version__ = importlib_metadata.version("sorl-thumbnail")
+try:
+    if sys.version_info >= (3, 8):  # Python 3.8+
+        import importlib.metadata as importlib_metadata
+    else:
+        import importlib_metadata
+
+    __version__ = importlib_metadata.version("sorl-thumbnail")
+except Exception:
+    pass
