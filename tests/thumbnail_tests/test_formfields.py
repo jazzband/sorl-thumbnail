9edbd2046cc6ac041e81b36cff6b0de253e67f9c
Camilo Nova <camilo.nova@axiacore.com>
2026-01-22 10:16:39 -0600

    feat: make ruff happy

--- tests/thumbnail_tests/test_formfields.py
+++ tests/thumbnail_tests/test_formfields.py
@@ -15,4 +15,7 @@ class ImageFormFieldTest(SimpleTestCase):
     def test_widget_attrs_default_accept(self):
         f = ImageFormField()
         self.assertEqual(f.widget_attrs(FileInput()), {'accept': 'image/*'})
-        self.assertWidgetRendersTo(f, '<input type="file" name="f" accept="image/*" required id="id_f" />')
+        self.assertWidgetRendersTo(
+            f,
+            '<input type="file" name="f" accept="image/*" required id="id_f" />'
+        )
